9d5a26a81fd9ed974d6426746b8702bdbde2f57b
Chris <44784345+chrisalexander0617@users.noreply.github.com>
2021-10-28 11:06:01 -0400

    Added container, fixed padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,20 +3,21 @@ import React from 'react';
 function Footer(props){
     return (
         <>
-            <footer className={`flex  justify-between p-10 w-full ${props.bg}`}>
-                <div>
-                    <div className={`font-bold ${props.textColor}`}>Digyt</div>    
-                </div>
-                <div>
-                    <ul className="flex flex-wrap">
-                        <li className="text-gray-900 mx-5 font-bold"></li>
-                        <li className="text-gray-900 mx-5 font-bold"></li>
-                        <li className="text-gray-900 mx-5 font-bold"></li>
-                        <li className="text-gray-900 mx-5 font-bold"></li>
-                    </ul>
-                </div>
-                
-            </footer>
+            <div className={`${props.bg} px-10`}>
+                <footer className={`md:container mx-auto flex justify-between py-10 px-0 w-full`}>
+                    <div>
+                        <div className={`font-bold ${props.textColor}`}>Digyt</div>    
+                    </div>
+                    <div>
+                        <ul className="flex flex-wrap">
+                            <li className="text-gray-900 mx-5 font-bold"></li>
+                            <li className="text-gray-900 mx-5 font-bold"></li>
+                            <li className="text-gray-900 mx-5 font-bold"></li>
+                            <li className="text-gray-900 mx-5 font-bold"></li>
+                        </ul>
+                    </div>
+                </footer>
+            </div>
         </>
     )
 }
